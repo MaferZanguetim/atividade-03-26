83b2b0c39605cc6103eeaea080d1d269a00bfbf5
Mafer Zanguetim <mafer.zanguetimcontatos@gmail.com>
2024-04-01 16:58:42 +0000

    Finalizando os Controllers

--- app/src/main/java/application/controller/TarefaController.java
+++ app/src/main/java/application/controller/TarefaController.java
@@ -3,9 +3,12 @@ package application.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -29,5 +32,24 @@ public class TarefaController {
     }
 
     @PostMapping
-    public Tarefa post()
+    public Tarefa post(@RequestBody Tarefa tarefa){
+        return tarefaRepo.save(tarefa);
+    }
+
+    @PutMapping("/{id}")
+    public Tarefa putTarefa(
+        @RequestBody Tarefa tarefa,
+        @PathVariable Long id){
+            Tarefa resposta = tarefaRepo.findById(id).get();
+            resposta.setDescricao(tarefa.getDescricao());
+            resposta.setConcluido(false);
+            resposta.setConcluido(true);
+
+            return tarefaRepo.save(resposta);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteTarefas(@PathVariable Long id){
+        tarefaRepo.deleteById(id);
+    }
 }
